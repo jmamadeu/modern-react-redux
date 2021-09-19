d9c8ad720a062b9cb5d19f71ec6be6fe0f1aa57c
jmamadeu <jmamadeu2000@gmail.com>
2021-09-19 05:32:24 +0100

    chore: add provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './app/store';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
