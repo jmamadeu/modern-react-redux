c2e1ea4bce965355bcc4ea0345d1aba371c77de7
jmamadeu <jmamadeu2000@gmail.com>
2021-09-19 05:33:08 +0100

    chore: counter using redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { incremented, amountAdded } from './features/counter/counter-slice'
+
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  function handleClick() {
+    dispatch(amountAdded(3))
+  }
 
   return (
     <div className="App">
@@ -11,7 +19,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={handleClick}>
             count is: {count}
           </button>
         </p>
