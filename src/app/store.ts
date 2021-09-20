acd98be2e364a10b8d51cbf2407045fcc98c701e
jmamadeu <jmamadeu2000@gmail.com>
2021-09-20 01:11:59 +0100

    chore: add api slice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import counterReducer from '../features/counter/counter-slice'
 
+import { apiSlice } from '../features/dogs/dogs-slice-api'
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware)
+  }
 })
 
 export type AppDispatch = typeof store.dispatch
