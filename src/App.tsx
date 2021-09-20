3fe73c8fa76a5d4fcc3282ca1d80d0b74e72bd41
jmamadeu <jmamadeu2000@gmail.com>
2021-09-20 01:12:51 +0100

    chore: fetch some data with redux fetch service

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,91 @@
-import React, { useState } from 'react'
-import { useAppDispatch, useAppSelector } from './app/hooks'
-import { incremented, amountAdded } from './features/counter/counter-slice'
+import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './features/counter/counter-slice';
 
-import logo from './logo.svg'
-import './App.css'
+import { useFetchBreedQuery } from './features/dogs/dogs-slice-api';
+
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value)
-  const dispatch = useAppDispatch()
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [] } = useFetchBreedQuery(numDogs);
 
   function handleClick() {
-    dispatch(amountAdded(3))
+    dispatch(amountAdded(3));
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={handleClick}>
+          <button type='button' onClick={handleClick}>
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+
+        <div>
+          <p>Dogs to fetch:</p>
+
+          <select
+            name='option'
+            value={numDogs}
+            onChange={(e) => {
+              setNumDogs(Number(e.target.value));
+            }}>
+            <option value='5'>5</option>
+            <option value='10'>10</option>
+            <option value='15'>15</option>
+            <option value='20'>20</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td> {breed.name} </td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'>
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'>
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
